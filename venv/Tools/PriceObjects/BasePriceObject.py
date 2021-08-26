c75b9bd20e45ff7c338efd830930acafe866084c
emiliacrow <emilia@franklinyoung.com>
2021-08-26 15:35:08 -0700

    Made GSA pricing one step.

--- venv/Tools/PriceObjects/BasePriceObject.py
+++ venv/Tools/PriceObjects/BasePriceObject.py
@@ -120,26 +120,30 @@ class BasePrice(BasicProcessObject):
         if 'Vendor List Price' in row and 'Discount' in row:
             vendor_list_price = float(row['Vendor List Price'])
             fy_discount_percent = float(row['Discount'])
-
-            # discount and cost
-            fy_cost_test = vendor_list_price - round((vendor_list_price * fy_discount_percent), 2)
-            check_val = abs(fy_cost_test - fy_cost)
-            # we trust the cost provided over the discount given
-            if check_val > 0.01:
-                fy_discount_percent = (1 - (fy_cost / vendor_list_price)) * 100
-                df_collect_product_base_data['Discount'] = [fy_discount_percent]
+            if fy_discount_percent != 0:
+                # discount and cost
+                fy_cost_test = vendor_list_price - round((vendor_list_price * fy_discount_percent), 2)
+                check_val = abs(fy_cost_test - fy_cost)
+                # we trust the cost provided over the discount given
+                if check_val > 0.01:
+                    fy_discount_percent = (1 - (fy_cost / vendor_list_price)) * 100
+                    df_collect_product_base_data['Discount'] = [fy_discount_percent]
 
         elif 'Discount' in row:
             fy_discount_percent = float(row['Discount'])
             if fy_discount_percent != 0:
                 vendor_list_price = round(fy_cost/(1-fy_discount_percent),2)
                 df_collect_product_base_data['Vendor List Price'] = [vendor_list_price]
+            else:
+                df_collect_product_base_data['Vendor List Price'] = [0]
 
         elif 'Vendor List Price' in row:
             vendor_list_price = float(row['Vendor List Price'])
             if vendor_list_price != 0:
                 fy_discount_percent = (1 - (fy_cost / vendor_list_price)) * 100
                 df_collect_product_base_data['Discount'] = fy_discount_percent
+            else:
+                df_collect_product_base_data['Discount'] = [0]
         else:
             df_collect_product_base_data['Discount'] = [0]
             df_collect_product_base_data['Vendor List Price'] = [0]
