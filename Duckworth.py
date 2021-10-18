5994f0a0bf1f04b6f0e6b3c782bb4005a73af020
emiliacrow <emilia@franklinyoung.com>
2021-10-18 11:42:45 -0700

    Limiting actions where depricated or disastrous.

--- Duckworth.py
+++ Duckworth.py
@@ -169,10 +169,15 @@ class DuckworthWindow(QWidget):
 
     def file_process_buttons(self, column_pos):
         # all the stuffing for file processing actions
-        self.file_action_options = ['Assign FyPartNumbers', 'File Merger Tool', 'Category Training',
+        self.all_file_action_options = ['Assign FyPartNumbers', 'File Merger Tool', 'Category Training',
                                     'Category Assignment', 'Category Management', 'Extract Attributes',
                                     'Unicode Correction', 'Generate Upload File', 'File Splitter Tool',
                                     'Load Image Files', 'Product Action Review']
+
+        self.file_action_options = ['Assign FyPartNumbers', 'File Merger Tool', 'Category Training',
+                                    'Category Assignment', 'Extract Attributes',
+                                    'Unicode Correction', 'Generate Upload File', 'File Splitter Tool',
+                                    'Product Action Review']
 
         self.file_action_options.sort()
 
