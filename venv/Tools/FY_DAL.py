acd106971bd43ca53ba9a8f59b3c891d8f64455f
emiliacrow <emilia@franklinyoung.com>
2022-11-10 12:34:03 -0800

    FyCOO can be A1 if it's a new ingestion.

--- venv/Tools/FY_DAL.py
+++ venv/Tools/FY_DAL.py
@@ -894,8 +894,8 @@ class DalObject:
         runner.start()
 
     def fy_product_description_insert(self, lst_descriptions):
-        proc_name = 'sequoia.ProductDescription_insert8'
-        proc_statement = 'CALL `sequoia`.`ProductDescription_insert8`(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);'
+        proc_name = 'sequoia.ProductDescription_insert7'
+        proc_statement = 'CALL `sequoia`.`ProductDescription_insert7`(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);'
         self.open_connection(runner_limit = 15)
         runner = DataRunner(self.connection, proc_name, proc_statement, lst_descriptions)
         runner.start()
