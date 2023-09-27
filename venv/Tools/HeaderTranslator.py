2a44e64aad20de516cb01886cfd7ddb98af62456
Emilia Crow <emilia@franklinyoung.com>
2023-09-27 13:15:29 -0700

    Distinguishing the IsHazardous and Fy values.

--- venv/Tools/HeaderTranslator.py
+++ venv/Tools/HeaderTranslator.py
@@ -59,7 +59,7 @@ class HeaderTranslator():
                             'UnitOfIssue':['UnitOfIssue', 'Unit Of Issue', 'UnitOfIssueSymbol', 'Unit Of Issue Symbol', 'UOI', 'uoi','unit_of_issue','unit of issue'],
                             'UnitOfMeasure':['UnitOfMeasure','Unit Of Measure','UnitOfMeasureSymbol','Unit Of Measure Symbol','UOM','uom','unit_of_measure','unit of measure','uom_std','UOM - STD','Unit code for Pack Quantity'],
 
-                            'IsHazardous': ['Is Hazardous', 'Hazardous', 'is_hazardous'],
+                            'IsHazardous': ['IsHazardous','Is Hazardous', 'Hazardous', 'is_hazardous'],
                             'FyIsHazardous':['FyIsHazardous','fy_is_hazardous'],
                             'HazardCode':['HazardCode','Hazard Code','Hazardous Code'],
                             'LeadTime':['LeadTimes','LeadTime','LeadTimeDays','Lead Time Days','Lead Time','lead_time_days', 'Lead Times', 'Lead Time', 'lead_time'],
